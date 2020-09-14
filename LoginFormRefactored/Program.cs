e77ef70a4a30eec561c192a80f5bb63d0d9d4ef1
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:20:53 +0300

    Week3-Logical-Operators

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your login and password.");
+            string login, password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password:");
+            password = Console.ReadLine();
+
+            if(login == "admin" && password == "pass1234")
+            {
+                Console.WriteLine("Welcome, Admin!");
+            }else
+            {
+                Console.WriteLine("Login failed. Please try again");
+            }
+        }
+    }
+}
---
+++ Week3LogicalOperators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week3LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your login and password.");
+            string login;
+            string password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password:");
+            password = Console.ReadLine();
+
+            if((login == "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Welcome!");
+            } else if((login == "admin") && (password != "pass1234"))
+            {
+                Console.WriteLine("The passworld is incorrect.");
+            } else if((login != "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("The login is incorrect.");
+            }else if ((login != "admin") && (password != "pass1234"))
+            {
+                Console.WriteLine("The login and the password are both incorrect.");
+            }
+        }
+    }
+}
